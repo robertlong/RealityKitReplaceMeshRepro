12493f9014c852a718d9529490060c142f4cd0db
Robert Long <robert@robertlong.me>
2023-08-31 11:37:01 -0700

    Preallocate arrays

--- ReplaceMeshRepro/ReplaceMeshReproApp.swift
+++ ReplaceMeshRepro/ReplaceMeshReproApp.swift
@@ -39,8 +39,20 @@ class DynamicMeshComponent: Component {}
 class DynamicMeshSystem: System {
     
     static let query = EntityQuery(where: .has(DynamicMeshComponent.self))
+    
+    // Modify to increase mesh complexity
+    static let divisions = 50
+    
+    var descriptor = MeshDescriptor()
+    var positions: [SIMD3<Float>]
+    var normals: [SIMD3<Float>]
+    var indices: [UInt32]
 
-    required init(scene: RealityKit.Scene) {}
+    required init(scene: RealityKit.Scene) {
+        positions = Array(repeating: SIMD3<Float>(), count: (Self.divisions + 1) * (Self.divisions + 1))
+        normals = Array(repeating: SIMD3<Float>(), count: (Self.divisions + 1) * (Self.divisions + 1))
+        indices = Array(repeating: 0, count: Self.divisions * Self.divisions * 6)
+    }
     
     func update(context: SceneUpdateContext) {
         for entity in context.scene.performQuery(Self.query) {
@@ -49,44 +61,37 @@ class DynamicMeshSystem: System {
             let time = CACurrentMediaTime()
             
             let radius = Float((sin(time) + 1.0) / 2.0)
-            let divisions = 10 // Modify to increase mesh complexity
             
-            var descriptor = MeshDescriptor()
-            
-            var positions: [SIMD3<Float>] = []
-            var normals: [SIMD3<Float>] = []
-            var indices: [UInt32] = []
-            
-            for i in 0...divisions {
-                let latitude = Float(i) * Float.pi / Float(divisions)
+            for i in 0...Self.divisions {
+                let latitude = Float(i) * Float.pi / Float(Self.divisions)
                 let y = radius * cos(latitude)
                 let r = radius * sin(latitude)
                 
-                for j in 0...divisions {
-                    let longitude = Float(j) * 2 * Float.pi / Float(divisions)
+                for j in 0...Self.divisions {
+                    let longitude = Float(j) * 2 * Float.pi / Float(Self.divisions)
                     let x = r * sin(longitude)
                     let z = r * cos(longitude)
-                    
                     let position = SIMD3<Float>(x, y, z)
-                    positions.append(position)
                     
-                    let normal = normalize(position)
-                    normals.append(normal)
+                    let index = i * (Self.divisions + 1) + j
+                    positions[index] = position
+                    normals[index] = normalize(position)
                 }
             }
             
-            for i in 0..<divisions {
-                for j in 0..<divisions {
-                    let first = i * (divisions + 1) + j
-                    let second = first + divisions + 1
+            for i in 0..<Self.divisions {
+                for j in 0..<Self.divisions {
+                    let indexOffset = i * Self.divisions * 6 + j * 6
+                    let first = i * (Self.divisions + 1) + j
+                    let second = first + Self.divisions + 1
                     
-                    indices.append(UInt32(first))
-                    indices.append(UInt32(second))
-                    indices.append(UInt32(first + 1))
+                    indices[indexOffset] = UInt32(first)
+                    indices[indexOffset + 1] = UInt32(second)
+                    indices[indexOffset + 2] = UInt32(first + 1)
                     
-                    indices.append(UInt32(second))
-                    indices.append(UInt32(second + 1))
-                    indices.append(UInt32(first + 1))
+                    indices[indexOffset + 3] = UInt32(second)
+                    indices[indexOffset + 4] = UInt32(second + 1)
+                    indices[indexOffset + 5] = UInt32(first + 1)
                 }
             }
             
